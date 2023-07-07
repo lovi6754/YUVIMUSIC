28bc5ad9056b6d49af7600481a1d506ec56181d7
Rishii_x_Janni <118364756+Rishii7205@users.noreply.github.com>
2023-07-07 10:59:38 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -13,7 +13,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     anon = math.floor(percentage)
-        if 0 < anon <= 2::
+        if 0 < anon <= 2:
             elif 76 < anon < 77:
         bar = "ﮩ♡٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
     elif 77 <= anon < 78:
